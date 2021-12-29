0f28cfb0f8e2a03e0163649c31f643f436b4cc31
x <powturbo@gmail.com>
2021-12-29 16:25:39 +0100

    Config/Platform

--- conf.h
+++ conf.h
@@ -1,5 +1,5 @@
 /**
-    Copyright (C) powturbo 2013-2019
+    Copyright (C) powturbo 2013-2022
     GPL v2 License
 
     This program is free software; you can redistribute it and/or modify
@@ -39,8 +39,8 @@
 #define popcnt64(_x_)   __builtin_popcountll(_x_)
 
     #if defined(__i386__) || defined(__x86_64__)
-//__bsr32:     1:0,2:1,3:1,4:2,5:2,6:2,7:2,8:3,9:3,10:3,11:3,12:3,13:3,14:3,15:3,16:4,17:4,18:4,19:4,20:4,21:4,22:4,23:4,24:4,25:4,26:4,27:4,28:4,29:4,30:4,31:4,32:5
-//  bsr32: 0:0,1:1,2:2,3:2,4:3,5:3,6:3,7:3,8:4,9:4,10:4,11:4,12:4,13:4,14:4,15:4,16:5,17:5,18:5,19:5,20:5,21:5,22:5,23:5,24:5,25:5,26:5,27:5,28:5,29:5,30:5,31:5,32:6,
+//x,__bsr32:     1:0,2:1,3:1,4:2,5:2,6:2,7:2,8:3,9:3,10:3,11:3,12:3,13:3,14:3,15:3,16:4,17:4,18:4,19:4,20:4,21:4,22:4,23:4,24:4,25:4,26:4,27:4,28:4,29:4,30:4,31:4,32:5
+//  x,bsr32: 0:0,1:1,2:2,3:2,4:3,5:3,6:3,7:3,8:4,9:4,10:4,11:4,12:4,13:4,14:4,15:4,16:5,17:5,18:5,19:5,20:5,21:5,22:5,23:5,24:5,25:5,26:5,27:5,28:5,29:5,30:5,31:5,32:6,
 static inline int    __bsr32(               int x) {             asm("bsr  %1,%0" : "=r" (x) : "rm" (x) ); return x; }
 static inline int      bsr32(               int x) { int b = -1; asm("bsrl %1,%0" : "+r" (b) : "rm" (x) ); return b + 1; }
 static inline int      bsr64(uint64_t x          ) { return x?64 - __builtin_clzll(x):0; }
@@ -54,6 +54,7 @@ static inline uint64_t ror64(uint64_t x, int s) { asm ("rorq %%cl,%0" :"=r" (x) 
 static inline int    __bsr32(unsigned x          ) { return   31 - __builtin_clz(  x); }
 static inline int      bsr32(int x               ) { return x?32 - __builtin_clz(  x):0; }
 static inline int      bsr64(uint64_t x) { return x?64 - __builtin_clzll(x):0; }
+static inline int    __bsr64(uint64_t x          ) { return   63 - __builtin_clzll(x);   }
 
 static inline unsigned rol32(unsigned x, int s) { return x << s | x >> (32 - s); }
 static inline unsigned ror32(unsigned x, int s) { return x >> s | x << (32 - s); }
@@ -64,7 +65,7 @@ static inline unsigned ror64(unsigned x, int s) { return x >> s | x << (64 - s);
 #define ctz64(_x_) __builtin_ctzll(_x_)
 #define ctz32(_x_) __builtin_ctz(_x_)    // 0:32  ctz32(1<<a) = a (a=1..31)
 #define clz64(_x_) __builtin_clzll(_x_)
-#define clz32(_x_) __builtin_clz(_x_)
+#define clz32(_x_) __builtin_clz(_x_)    // 00000000 00000000 00000000 01000000 = 25
 
 //#define bswap8(x)    (x)
     #if __GNUC__ > 4 || __GNUC__ == 4 && __GNUC_MINOR__ >= 8
@@ -135,6 +136,8 @@ static inline int clz64(uint64_t x) { unsigned long z;   _BitScanReverse64(&z, x
 static inline double round(double num) { return (num > 0.0) ? floor(num + 0.5) : ceil(num - 0.5); }
   #endif
 
+#define __bsr8(_x_)  __bsr32(_x_)
+#define __bsr16(_x_) __bsr32(_x_)
 #define bsr8(_x_)  bsr32(_x_)
 #define bsr16(_x_) bsr32(_x_)
 #define ctz8(_x_)  ctz32(_x_)
@@ -216,42 +219,74 @@ struct _PACKED doubleu   { double             d; };
 #endif
 
 //---------------------misc ---------------------------------------------------
-#define BZHI64(_u_, _b_) ((_u_) & ((1ull<<(_b_))-1))
-#define BZHI32(_u_, _b_) ((_u_) & ((1u  <<(_b_))-1))
-#define BZHI16(_u_, _b_) BZHI32(_u_, _b_)
-#define BZHI8(_u_, _b_)  BZHI32(_u_, _b_)
+//#define bzhi63(_u_, _b_) 				((_u_) & ((1ull<<(_b_))-1))  // _b_ < 64
+//#define bzhi63(_u_, _b_)                ((_u_) & ((1u  <<(_b_))-1))  // _b_ < 32
+#define BZHI64(_u_, _b_)                 (_b_ == 64?0xffffffffffffffffull:((_u_) & ((1ull<<(_b_))-1)))  // b Constant
+#define BZHI32(_u_, _b_)                 (_b_ == 32?        0xffffffffu  :((_u_) & ((1u  <<(_b_))-1)))  
+#define BZHI16(_u_, _b_)                 BZHI32(_u_, _b_)
+#define BZHI8( _u_, _b_)                 BZHI32(_u_, _b_)
+
+    #ifdef __AVX2__
+      #if defined(_MSC_VER) && !defined(__INTEL_COMPILER)
+#include <intrin.h>
+      #else
+#include <x86intrin.h>
+      #endif
+#define bzhi32(_u_, _b_)                 _bzhi_u32(_u_, _b_)  // b variable
+#define bzhi31(_u_, _b_)                 _bzhi_u32(_u_, _b_)
+
+      #if !(defined(_M_X64) || defined(__amd64__)) && (defined(__i386__) || defined(_M_IX86))
+#define bzhi64(_u_, _b_)                 BZHI64(_u_, _b_)
+#define bzhi63(_u_, _b_)                 ((_u_) & ((1ull<<(_b_))-1))
+      #else
+#define bzhi64(_u_, _b_)                 _bzhi_u64(_u_, _b_)
+#define bzhi63(_u_, _b_)                 _bzhi_u64(_u_, _b_)
+      #endif
+    #else
+#define bzhi64(_u_, _b_)                 BZHI64(_u_, _b_) 
+#define bzhi63(_u_, _b_)                 ((_u_) & ((1ull <<(_b_))-1)) 
+#define bzhi32(_u_, _b_)                 ((_u_) & ((1ull <<(_b_))-1))
+#define bzhi31(_u_, _b_)                 ((_u_) & ((1    <<(_b_))-1))
+    #endif
+
+#define bzhi16(_u_, _b_)                 bzhi31(_u_, _b_)
+#define bzhi8( _u_, _b_)                 bzhi31(_u_, _b_)
+
 
 #define SIZE_ROUNDUP(_n_, _a_) (((size_t)(_n_) + (size_t)((_a_) - 1)) & ~(size_t)((_a_) - 1))
 #define ALIGN_DOWN(__ptr, __a) ((void *)((uintptr_t)(__ptr) & ~(uintptr_t)((__a) - 1)))
 
 #define TEMPLATE2_(_x_, _y_) _x_##_y_
-#define TEMPLATE2(_x_, _y_) TEMPLATE2_(_x_,_y_)
+#define T2(_x_, _y_) TEMPLATE2_(_x_,_y_)
 
 #define TEMPLATE3_(_x_,_y_,_z_) _x_##_y_##_z_
-#define TEMPLATE3(_x_,_y_,_z_) TEMPLATE3_(_x_, _y_, _z_)
+#define T3(_x_,_y_,_z_) TEMPLATE3_(_x_, _y_, _z_)
 
 #define CACHE_LINE_SIZE     64
 #define PREFETCH_DISTANCE   (CACHE_LINE_SIZE*4)
+
+#define CLAMP(_x_, _low_, _high_)  (((_x_) > (_high_)) ? (_high_) : (((_x_) < (_low_)) ? (_low_) : (_x_)))
+
 //--- NDEBUG -------
 #include <stdio.h>
   #ifdef _MSC_VER
     #ifdef NDEBUG
 #define AS(expr, fmt, ...)
-#define AC(expr, fmt, ...) do { if(!(expr)) { fprintf(stderr, fmt, ##__VA_ARGS__ ); fflush(stderr); abort(); } } while(0)
+#define AC(expr, fmt, ...) do { if(!(expr)) { fprintf(stderr, fmt, ##__VA_ARGS__ ); fflush(stderr); exit(-1); } } while(0)
 #define die(fmt, ...) do { fprintf(stderr, fmt, ##__VA_ARGS__ ); fflush(stderr); exit(-1); } while(0)
     #else
-#define AS(expr, fmt, ...) do { if(!(expr)) { fflush(stdout);fprintf(stderr, "%s:%s:%d:", __FILE__, __FUNCTION__, __LINE__); fprintf(stderr, fmt, ##__VA_ARGS__ ); fflush(stderr); abort(); } } while(0)
-#define AC(expr, fmt, ...) do { if(!(expr)) { fflush(stdout);fprintf(stderr, "%s:%s:%d:", __FILE__, __FUNCTION__, __LINE__); fprintf(stderr, fmt, ##__VA_ARGS__ ); fflush(stderr); abort(); } } while(0)
+#define AS(expr, fmt, ...) do { if(!(expr)) { fflush(stdout);fprintf(stderr, "%s:%s:%d:", __FILE__, __FUNCTION__, __LINE__); fprintf(stderr, fmt, ##__VA_ARGS__ ); fflush(stderr); exit(-1); } } while(0)
+#define AC(expr, fmt, ...) do { if(!(expr)) { fflush(stdout);fprintf(stderr, "%s:%s:%d:", __FILE__, __FUNCTION__, __LINE__); fprintf(stderr, fmt, ##__VA_ARGS__ ); fflush(stderr); exit(-1); } } while(0)
 #define die(fmt, ...) do { fprintf(stderr, "%s:%s:%d:", __FILE__, __FUNCTION__, __LINE__); fprintf(stderr, fmt, ##__VA_ARGS__ ); fflush(stderr); exit(-1); } while(0)
     #endif
   #else
     #ifdef NDEBUG
 #define AS(expr, fmt,args...)
-#define AC(expr, fmt,args...) do { if(!(expr)) { fprintf(stderr, fmt, ## args ); fflush(stderr); abort(); } } while(0)
+#define AC(expr, fmt,args...) do { if(!(expr)) { fprintf(stderr, fmt, ## args ); fflush(stderr); exit(-1); } } while(0)
 #define die(fmt,args...) do { fprintf(stderr, fmt, ## args ); fflush(stderr); exit(-1); } while(0)
     #else
-#define AS(expr, fmt,args...) do { if(!(expr)) { fflush(stdout);fprintf(stderr, "%s:%s:%d:", __FILE__, __FUNCTION__, __LINE__); fprintf(stderr, fmt, ## args ); fflush(stderr); abort(); } } while(0)
-#define AC(expr, fmt,args...) do { if(!(expr)) { fflush(stdout);fprintf(stderr, "%s:%s:%d:", __FILE__, __FUNCTION__, __LINE__); fprintf(stderr, fmt, ## args ); fflush(stderr); abort(); } } while(0)
+#define AS(expr, fmt,args...) do { if(!(expr)) { fflush(stdout);fprintf(stderr, "%s:%s:%d:", __FILE__, __FUNCTION__, __LINE__); fprintf(stderr, fmt, ## args ); fflush(stderr); exit(-1); } } while(0)
+#define AC(expr, fmt,args...) do { if(!(expr)) { fflush(stdout);fprintf(stderr, "%s:%s:%d:", __FILE__, __FUNCTION__, __LINE__); fprintf(stderr, fmt, ## args ); fflush(stderr); exit(-1); } } while(0)
 #define die(fmt,args...) do { fprintf(stderr, "%s:%s:%d:", __FILE__, __FUNCTION__, __LINE__); fprintf(stderr, fmt, ## args ); fflush(stderr); exit(-1); } while(0)
     #endif
   #endif
