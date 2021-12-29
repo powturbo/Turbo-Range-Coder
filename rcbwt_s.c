decd553243fc2b6359989e5f34bfae876f0db1a0
x <powturbo@gmail.com>
2021-12-29 16:25:39 +0100

    TurboRC: bwt - predictor simple

---
+++ rcbwt_s.c
@@ -0,0 +1,32 @@
+/**
+    Copyright (C) powturbo 2013-2022
+    GPL v3 License
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+    - homepage : https://sites.google.com/site/powturbo/
+    - github   : https://github.com/powturbo
+    - twitter  : https://twitter.com/powturbo
+    - email    : powturbo [_AT_] gmail [_DOT_] com
+**/
+// TurboRC: Range Coder bwt - simple predictor (16 bits counters)
+#include <stdio.h>             
+#include "conf.h"
+
+#define RC_MACROS
+#define RC_BITS 10      	// RC_SIZE + RC_IO: set in turborc_.h
+#include "turborc_.h"
+#include "mbc_s.h"       	// simple predictor
+#include "rcbwt_.c"         // bwt template functions
