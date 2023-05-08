5113b4c849fdb9bd9fe2deccde1bc7f9050d5cea
x <powturbo@gmail.com>
2023-05-08 12:01:16 +0200

    rANS: Asymmetric Numeral Systems include

--- anscdf_.h
+++ anscdf_.h
@@ -55,12 +55,12 @@ typedef unsigned short io_t;
 #define ANS_LBITS                       (8*(sizeof(state_t) - sizeof(io_t)) - 1)
 #define ANS_LOW                         (1u << ANS_LBITS)
 
-#define _putc(_ch_, _out_) 	            _out_ -= sizeof(io_t),*(io_t *)_out_ = (_ch_)
-#define _getc(_in_) 		            *(io_t *)_in_;_in_+=sizeof(io_t)
+#define _putc(_ch_, _out_)              _out_ -= sizeof(io_t),*(io_t *)_out_ = (_ch_)
+#define _getc(_in_)                     *(io_t *)_in_;_in_+=sizeof(io_t)
 //------------------------------- Entropy coder -----------------------
-#define eceflush(_x_, _op_) 		{ _op_ -= sizeof(state_t); *(state_t *)_op_ = _x_; }
-#define ecdini(  _x_, _ip_) 		{ _x_   = *(state_t *)_ip_; _ip_ += sizeof(state_t); }
-#define ecenorm(_st_,_mbp_,_out_) 	if(_st_ >= (state_t)(_mbp_)<<(sizeof(state_t)*8-1-ANS_BITS)) { _putc(_st_, _out_); _st_ >>= IOBITS; }
+#define eceflush(_x_, _op_)         { _op_ -= sizeof(state_t); *(state_t *)_op_ = _x_; }
+#define ecdini(  _x_, _ip_)         { _x_   = *(state_t *)_ip_; _ip_ += sizeof(state_t); }
+#define ecenorm(_st_,_mbp_,_out_)   if(_st_ >= (state_t)(_mbp_)<<(sizeof(state_t)*8-1-ANS_BITS)) { _putc(_st_, _out_); _st_ >>= IOBITS; }
 
   #ifdef __x86_64 // https://gcc.godbolt.org/z/5fY1M6qsd
 #define ecdnorm(_x_,_ip_) { asm(\
@@ -109,48 +109,106 @@ typedef unsigned short mbu;
 #define ANSN           2 
 #define STATEDEF(_st_) state_t _st_[ANSN] = {ANS_LOW, ANS_LOW}
 
+#define ansenc(_m_, _sti_, _ep_, _x_) { unsigned _bp = _m_[_x_], _cbp = _m_[(_x_)+1] - _bp; ece(_sti_, _cbp, _bp, _ep_); }
+#define ansflush(_st_, _ep_) for(int _i = 0; _i < ANSN; _i++) eceflush(_st_[_i], _ep_)
+#define ansdec(_m_, _sti_, _ip_, _x_) do { cdf16sansdec(_m_, _sti_, _x_); ecdnorm(_sti_, _ip_); } while(0)
+	
 //-- encode
-#define mn4enc(_m_,_y_,_si_, _stk_) {\
-  unsigned _cp = _m_[_y_];                   /*AC(_stk_ + 2 <= _stk + blksize*4, "mn4enc:Fatal error");*/\
+#define mnenc4(_m_,_si_, _x_, _stk_) {\
+  unsigned _cp = _m_[_x_];                                                      /*AC(_stk_ + 2 <= _stk + blksize*4, "mnenc4:Fatal error");*/\
   *_stk_++ = _si_<<15 | _cp;\
-  *_stk_++ = _m_[(_y_)+1] - _cp;\
-  cdf16upd(_m_,_y_);\
+  *_stk_++ = _m_[(_x_)+1] - _cp;\
+  cdf16upd(_m_,_x_);\
 }
 
-#define mn8enc(_mh_, _ml_, _x_,_stk_) {\
+#define mnenc8(_mh_, _ml_, _x_,_stk_) {\
   unsigned _x = _x_, _yh = _x>>4, _yl = _x & 0xf;\
-  mn4enc(_mh_, _yh, 1, _stk_);\
+  mnenc4(_mh_, 1, _yh, _stk_);\
   mbu *_m = _ml_[_yh];\
-  mn4enc(_m, _yl, 0, _stk_);\
+  mnenc4(_m, 0, _yl, _stk_);\
 }
 
 #define mnflush(_op_,_op__,__stk_,_stk_) {\
-  unsigned char *_ep = _op__, _i; /*printf("stk=%d ", (_stk_ - __stk_)/sizeof(_stk[0]) );*/ \
   STATEDEF(_st);\
-  while(_stk_ != __stk_) { 							if(_ep < _op_+sizeof(io_t)+ANSN*sizeof(_st[0])) { memcpy(out,in,inlen); op=out+inlen; goto end; }\
-    unsigned _pb = *--_stk_, _si = *--_stk_, _cpb = _si&0x7fff;\
-	ece(_st[_si>>15], _pb, _cpb, _ep);                         \
-  }\
-  for(_i = 0; _i < ANSN; _i++)\
-    eceflush(_st[_i],_ep);\
-  int l = _op__-_ep;\
-  memcpy(_op_, _ep, l); \
-  _op_ += l;\
-}
+  unsigned char *_ep = _op__,_i;\
+  while(_stk_ != __stk_) {\
+    unsigned _pb = *--_stk_, _si = *--_stk_, _cpb = _si & 0x7fff;               if(_ep <= _op_+sizeof(io_t)+ANSN*sizeof(_st[0])) goto ovr;\
+    ece(_st[_si>>15], _pb, _cpb, _ep);\
+  }                                                                             \
+  for(_i = 0; _i < ANSN; _i++) eceflush(_st[_i], _ep);                          if(_ep <= _op_) goto ovr;\
+  unsigned _l = _op__ - _ep;                                                    if(_op_ + _l >= _op__) goto ovr;\
+  memmove(_op_, _ep, _l); _op_ += _l; \
+} 
 
 //-- Decode
-#define mn4dec(_m_,_y_,_si_, _st_) cdfansdec(_st_[_si_], _m_, _y_)
-#define mn4dec0(_ip_,_m_,_y_,_si_, _st_) do { mn4dec(_m_,_y_,_si_, _st_); ecdnorm(_st_[_si_], _ip_); } while(0)
-#define mn8dec(_mh_, _ml_, _x_, _st_) { \
+#define mndec4(_m_, _st_, _ip_, _x_) do { cdf16ansdec(_m_, _st_, _x_); ecdnorm(_st_, _ip_); } while(0)
+#define mndec8(_mh_, _ml_, _st_, _ip_, _x_) {\
   unsigned _yh,_yl;\
-  mn4dec(_mh_,_yh,0, _st_);\
+  cdf16ansdec(_mh_, _st_[0], _yh);\
   mbu *_m = _ml_[_yh];\
-  mn4dec(_m,_yl,1, _st_);\
+  cdf16ansdec(_m, _st_[1], _yl);\
   _x_ = _yh << 4| _yl;\
-  ecdnorm(_st_[0], ip);\
-  ecdnorm(_st_[1], ip);\
-}
+  ecdnorm(_st_[0], _ip_);\
+  ecdnorm(_st_[1], _ip_);\
+}  
+
+#define mndec8x(_mh_, _ml_, _st_, _ip_, _x_) {\
+  unsigned _yh,_yl;\
+  cdf16ansdec(_mh_, _st_[0], _yh);\
+  cdf16ansdec(_ml_[_yh], _st_[1], _yl);\
+  _x_ = _yh << 4| _yl;\
+  ecdnorm(_st_[0], _ip_);\
+  ecdnorm(_st_[1], _ip_);\
+}  
 
 #define mnfill(_st_, _ip_) {\
   unsigned _i; for(_i = 0; _i < ANSN; _i++) { if(_st_[_i] != ANS_LOW) die("Fatal error: st=%X\n", _st_[_i]); ecdini(_st_[_i], _ip_); }\
 }  
+
+// Variable Length Coding: Integer 0-299
+// 1:0-12   		   0-12
+// 2:13,14 xxxx        13,14...45(13+32)
+// 3:15	   xxxx+xxxx   46,47..299(255+13+32)
+#define cdfenc8(_m0_, _m1_, _m2_, _x_, _stk_) { unsigned _x = _x_;\
+  if(likely(_x < 13))   {                                        mnenc4(_m0_, 0, _x, _stk_); }\
+  else if  (_x < 13+32) { _x -= 13;    unsigned _y = (_x>>4)+13; mnenc4(_m0_, 0, _y, _stk_);  \
+                                                _y = _x&0xf;     mnenc4(_m1_, 1, _y, _stk_); }\
+  else {                  _x -= 13+32; unsigned _y = _x>>4;      mnenc4(_m0_, 0, 15, _stk_);  \
+                                                                 mnenc4(_m1_, 1, _y, _stk_);  \
+											    _y = _x&0xf;     mnenc4(_m2_, 0, _y, _stk_); }\
+}
+
+#define cdfdec8(_m0_, _m1_, _m2_, _st_, _ip_, _x_) {\
+                                 mndec4(_m0_, _st_[0], _x_,_ip0_);\
+  if(_x_ >= 13) {\
+    if(_x_ != 15) { unsigned _y; mndec4(_m1_, _st_[1], _ip_, _y); _x_ = ((_x_-13)<<4|_y)+13; }\
+    else {          unsigned _y; mndec4(_m1_, _st_[1], _ip_, _y);\
+	                             mndec4(_m2_, _st_[0], _ip_, _x_); _x_ = (_y<<4|_x_)+13+32; }\
+  }\
+}
+// 7bits: 8+127
+//1: 0-8                            xxxx 
+//2: 9+3bits: 9,10,11, 12,13,14,15  1xxx xxxx 
+#define cdfenc7(_m0_, _m1_, _x_, _stk_) { unsigned _x = _x_;\
+  if(likely(_x < 8))    {                  mnenc4(_m0_, 1, _x, _stk_); }\
+  else { _x -= 8; unsigned _y = (_x>>4)+8; mnenc4(_m0_, 1, _y, _stk_); \
+                           _y = _x&0xf;    mnenc4(_m1_, 0, _y, _stk_); }\					   
+}
+
+#define cdfdec7(_m0_, _m1_, _st_, _ip_, _x_) {\
+                              mndec4(_m0_, _st_[0], _ip_, _x_);\
+  if(_x_ >= 8) { unsigned _y; mndec4(_m1_, _st_[1], _ip_, _y); _x_ = ((_x_-8)<<4|_y)+8; }\
+}
+// 6 bits: 0-76
+// 1: 0-12 
+// 2: 13,14,15: bbxxxx 13 - 13+63 bits
+#define cdfenc6(_m0_, _m1_, _x_, _stk_) { unsigned _x = _x_;\
+  if(likely(_x < 12))    {                   mnenc4(_m0_, 1, _x, _stk_); }\
+  else { _x -= 12; unsigned _y = (_x>>4)+12; mnenc4(_m0_, 1, _y, _stk_);\
+                            _y = _x&0xf;     mnenc4(_m1_, 0, _y, _stk_); }\
+}
+
+#define cdfdec6(_m0_, _m1_, _st_, _ip_, _x_) {\
+                               mndec4(_m0_, _st_[0], _ip_, _x_);\
+  if(_x_ >= 12) { unsigned _y; mndec4(_m1_, _st_[1], _ip_, _y ); _x_=((_x_-12)<<4|_y)+12; }\
+}
