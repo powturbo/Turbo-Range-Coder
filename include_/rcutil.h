9e4cfe8e186201d7af7aa9ea62458f6de2bad59a
x <powturbo@gmail.com>
2023-08-06 17:25:45 +0200

    Rc utility

--- include_/rcutil.h
+++ include_/rcutil.h
@@ -33,16 +33,19 @@ typedef unsigned long long uint64_t;
 #include <stdint.h>
 #endif
 #include <stddef.h>
-/* #define __STDC_WANT_IEC_60559_TYPES_EXT__
+
+#define __STDC_WANT_IEC_60559_TYPES_EXT__
 #include <float.h>
 
 #if defined(__clang__) && defined(__is_identifier)
   #if !__is_identifier(_Float16)
-    #undef FLT16_BUILTIN
+    #undef HAVE_FLOAT16
   #endif
-#elif defined(FLT16_MAX) || defined(__HAVE_FLOAT16) 
-#define FLT16_BUILTIN
-#endif*/
+#elif defined(FLT16_MAX)
+#define HAVE_FLOAT16
+#elif __HAVE_FLOAT16 
+#define HAVE_FLOAT16
+#endif
 
 #ifdef __cplusplus
 extern "C" {
@@ -97,7 +100,7 @@ void zzagenc64(   unsigned char *in, size_t inlen, unsigned char *out);
 void zzagdec64(   unsigned char *in, size_t inlen, unsigned char *out);
 
 //------- Quantization [0..qmax] : qmax maximum quantized value in out ----------------
-  #if defined(FLT16_BUILTIN) 
+  #if defined(HAVE_FLOAT16) 
 size_t fpquant8e16( _Float16 *in, size_t inlen, uint8_t  *out, unsigned qmax, _Float16 *pfmin, _Float16 *pfmax, _Float16 zmin);
 size_t fpquant16e16(_Float16 *in, size_t inlen, uint16_t *out, unsigned qmax, _Float16 *pfmin, _Float16 *pfmax, _Float16 zmin);
 //size_t fpquantv8e16(_Float16 *in, size_t inlen, uint8_t  *out, unsigned qmax, _Float16 *pfmin, _Float16 *pfmax, _Float16 zmin);                                                                                                                             
@@ -112,7 +115,7 @@ size_t fpquant16e64(   double *in, size_t inlen, uint16_t *out, unsigned qmax, d
 size_t fpquant32e64(   double *in, size_t inlen, uint32_t *out, unsigned qmax, double   *pfmin,   double *pfmax,  double zmin);
 size_t fpquant64e64(   double *in, size_t inlen, uint64_t *out, unsigned qmax, double   *pfmin,   double *pfmax,  double zmin);
 
-    #if defined(FLT16_BUILTIN) 
+    #if defined(HAVE_FLOAT16) 
 size_t fpquant8d16(  uint8_t  *in, size_t outlen, _Float16 *out, unsigned qmax, _Float16   fmin, _Float16   fmax, size_t inlen);
 size_t fpquant16d16( uint16_t *in, size_t outlen, _Float16 *out, unsigned qmax, _Float16   fmin, _Float16   fmax);
 //size_t fpquantv8d16( uint8_t  *in, size_t outlen, _Float16 *out, unsigned qmax, _Float16   fmin, _Float16   fmax);
@@ -129,7 +132,7 @@ size_t fpquant64d64( uint64_t *in, size_t outlen, double   *out, unsigned qmax, 
 
 //------- Lossy floating point transform: pad the trailing mantissa bits with zeros according to the error e (ex. e=0.00001)
 // must include float.h to use _Float16 (see icapp.c)
- #ifdef FLT16_BUILTIN
+ #ifdef HAVE_FLOAT16
 _Float16 _fprazor16(_Float16 d, float e, int lg2e);  
 void fprazor16(_Float16 *in, unsigned n, _Float16  *out, float  e);
   #endif
